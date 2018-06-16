156b63ea03e399b76de1087da27f86997cd21370
SkalskiP <pskalski@student.agh.edu.pl>
2018-06-16 23:59:53 +0200

    functions

--- crashCourse/haskell-tut-04.hs
+++ crashCourse/haskell-tut-04.hs
@@ -1,3 +1,31 @@
 import Data.List
 import System.IO
 
+-- addMe function
+-- type declaration
+addMe :: Int -> Int -> Int
+-- funcName param1 param2 = operations (returned value)
+addMe x y = x + y
+
+-- ------------------------------------------------------------------------
+
+addTyples :: (Int, Int) -> (Int, Int) -> (Int, Int)
+addTyples (x, y) (x2, y2) = (x + x2, y + y2)
+
+-- ------------------------------------------------------------------------
+
+whatAge :: Int -> String
+whatAge 16 = "You can drive"
+whatAge 18 = "You can vote"
+whatAge 21 = "You're an adult"
+whatAge _ = "Nothing Important"
+
+-- ------------------------------------------------------------------------
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+-- ------------------------------------------------------------------------
+
+prodFact n = product [1..n]
